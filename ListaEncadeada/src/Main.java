2e1ba683d25039a779853aa2b1e8d42dd3a06e81
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2025-07-18 22:38:54 -0300

    Lista encadeada

--- ListaEncadeada/src/Main.java
+++ ListaEncadeada/src/Main.java
@@ -1,5 +1,11 @@
 public class Main {
     public static void main(String[] args) {
-        
+        Lista lista = new Lista();
+        lista.inserir(1, 4);
+        lista.inserir(1, 5);
+        lista.inserir(1, 8);
+        lista.exibirLista();
+        lista.remove(2);
+        lista.exibirLista();
     }
 }
