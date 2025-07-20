4b7c761775668b5a6b77c7ec0c6ae2b846341a0e
joaoVitor-amaro <joaovitoramaro71@gmail.com>
2025-07-19 21:44:34 -0300

    Feat: Criado o teste da lista

--- ListaEncadeada/src/Main.java
+++ ListaEncadeada/src/Main.java
@@ -1,11 +1,23 @@
 public class Main {
     public static void main(String[] args) {
         Lista lista = new Lista();
-        lista.inserir(1, 4);
-        lista.inserir(1, 5);
-        lista.inserir(1, 8);
+        lista.inserir(1, 2);
+        lista.inserir(2, 5);
+        lista.inserir(3, 8);
         lista.exibirLista();
-        lista.remove(2);
+        lista.remove(3);
         lista.exibirLista();
+        System.out.println(lista.getTamanho());
+        lista.inserir(2, 10);
+        lista.exibirLista();
+        lista.modificarValor(2, 15);
+        lista.exibirLista();
+        int valorList = lista.obterValor(2);
+        if(valorList != -1) {
+            System.out.printf("Valor buscado: %d\n", valorList);
+        } else {
+            System.out.println("Valor não encontrado");
+        }
+        System.out.printf("Tamanho da lista: %d\n", lista.getTamanho());
     }
 }
